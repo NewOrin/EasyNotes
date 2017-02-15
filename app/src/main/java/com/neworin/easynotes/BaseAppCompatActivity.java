3c736987b95764ff0d5f6c38be4bb795743a5ac1
NewOrin(张富斌) <neworin@163.com>
2017-02-15 17:33:06 +0800

    修改app主题文件; 创建BaseAppCompatActivity文件; 主页面添加Toolbar; 完成主页面部分布局

---
+++ app/src/main/java/com/neworin/easynotes/BaseAppCompatActivity.java
@@ -0,0 +1,50 @@
+package com.neworin.easynotes;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+/**
+ * Created by NewOrin Zhang
+ * 02/15/2017
+ * Project: EasyNotes
+ * Email: neworin@163.com
+ */
+
+public abstract class BaseAppCompatActivity extends AppCompatActivity {
+
+    private static final String TAG = BaseAppCompatActivity.class.getSimpleName();
+    private Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutId());
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (mToolbar != null) {
+            //将Toolbar显示到界面
+            setSupportActionBar(mToolbar);
+        }
+    }
+
+    /**
+     * this Activity of tool bar.
+     * 获取头部.
+     *
+     * @return support.v7.widget.Toolbar.
+     */
+    public Toolbar getToolbar() {
+        return (Toolbar) findViewById(R.id.toolbar);
+    }
+
+    /**
+     * this activity layout res
+     * 设置layout布局,在子类重写该方法.
+     *
+     * @return res layout xml id
+     */
+    protected abstract int getLayoutId();
+
+    
+}
--- app/src/main/java/com/neworin/easynotes/MainActivity.java
+++ app/src/main/java/com/neworin/easynotes/MainActivity.java
@@ -1,13 +1,54 @@
 package com.neworin.easynotes;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseAppCompatActivity {
+
+    private LinearLayout mMainLeftContainerLl;
+    private FrameLayout mMainContentContainer;
+    private DrawerLayout mMainDrawerlayout;
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(getLayoutId());
+        initView();
+    }
+
+    private void initView() {
+        getToolbar().setTitle(getString(R.string.main_default_title));
+        mMainLeftContainerLl = (LinearLayout) findViewById(R.id.main_left_container_ll);
+        mMainContentContainer = (FrameLayout) findViewById(R.id.main_content_container);
+        mMainDrawerlayout = (DrawerLayout) findViewById(R.id.main_drawerlayout);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ActionBarDrawerToggle
+                actionBarDrawerToggle =
+                new ActionBarDrawerToggle(this,
+                                          mMainDrawerlayout,
+                                          getToolbar(),
+                                          R.string.app_name,
+                                          R.string.close);
+        actionBarDrawerToggle.syncState();
+        mMainDrawerlayout.setDrawerListener(actionBarDrawerToggle);
+        getToolbar().inflateMenu(R.menu.main_menu);
+        getToolbar().setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return true;
+            }
+        });
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_main;
     }
 }
