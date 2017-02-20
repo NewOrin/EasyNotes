03da1d45a4719b43d983a246eff2f699f895ee16
NewOrin <neworin@163.com>
2017-02-20 09:03:12 +0800

    完善侧滑菜单布局

--- app/src/main/java/com/neworin/easynotes/SlideMenuFragment.java
+++ app/src/main/java/com/neworin/easynotes/SlideMenuFragment.java
@@ -38,11 +38,14 @@ public class SlideMenuFragment extends BaseFragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         List<SlideMenuNoteBookModel> datas = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 3; i++) {
             datas.add(new SlideMenuNoteBookModel("笔记本", "2", false));
         }
         ListViewCommonAdapter<SlideMenuNoteBookModel> adapter = new ListViewCommonAdapter<>(getActivity(), datas, R.layout.item_slide_menu_layout, com.neworin.easynotes.BR.notebookBean);
         binding.setAdapter(adapter);
-//        binding.slideMenuListview.setAdapter(adapter);
+        View footViewSettings = LayoutInflater.from(getActivity()).inflate(R.layout.slide_menu_footer_settings_layout, null);
+        View footViewEdit = LayoutInflater.from(getActivity()).inflate(R.layout.slide_menu_footer_edit_layout, null);
+        binding.slideMenuListview.addFooterView(footViewSettings);
+        binding.slideMenuListview.addFooterView(footViewEdit);
     }
 }
