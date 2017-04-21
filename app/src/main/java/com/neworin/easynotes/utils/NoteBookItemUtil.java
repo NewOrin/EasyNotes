49e663f1c02cc27531121f94983f46bf190a3f3c
NewOrin <neworin@163.com>
2017-04-21 14:59:10 +0800

    完成首页笔记本列表展示

---
+++ app/src/main/java/com/neworin/easynotes/utils/NoteBookItemUtil.java
@@ -0,0 +1,68 @@
+package com.neworin.easynotes.utils;
+
+import android.databinding.BindingAdapter;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.alibaba.fastjson.JSON;
+import com.bumptech.glide.Glide;
+import com.neworin.easynotes.model.EditData;
+
+import java.util.List;
+
+/**
+ * Created by NewOrin Zhang on 2017/4/21.
+ * Project : com.neworin.easynotes.utils
+ * Description:
+ */
+
+public class NoteBookItemUtil {
+
+    public static boolean isShow(String content) {
+        List<EditData> list = JSON.parseArray(content, EditData.class);
+        for (EditData e : list) {
+            if (e.getImagePath() != null) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 设置列表图片
+     *
+     * @param imageView
+     * @param content
+     */
+    @BindingAdapter("imageUrl")
+    public static void setImageUrl(ImageView imageView, String content) {
+        List<EditData> list = JSON.parseArray(content, EditData.class);
+        if (null == list || list.size() == 0) {
+            imageView.setVisibility(View.GONE);
+            return;
+        }
+        for (EditData e : list) {
+            if (e.getImagePath() != null) {
+                Glide.with(imageView.getContext()).load(e.getImagePath()).into(imageView);
+                return;
+            }
+        }
+    }
+
+    @BindingAdapter("textView")
+    public static void setTextView(TextView textView, String content) {
+        StringBuilder sb = new StringBuilder();
+        List<EditData> list = JSON.parseArray(content, EditData.class);
+        if (null == list || list.size() == 0) {
+            textView.setVisibility(View.GONE);
+            return;
+        }
+        for (EditData e : list) {
+            if (e.getImagePath() == null) {
+                sb.append(e.getInputStr());
+            }
+        }
+        textView.setText(sb.toString());
+    }
+}
