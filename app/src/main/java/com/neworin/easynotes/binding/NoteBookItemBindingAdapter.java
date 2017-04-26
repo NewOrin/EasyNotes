b2e396474591be4630dc3330f1543c653356cc82
NewOrin <neworin@163.com>
2017-04-26 18:40:16 +0800

    完善笔记列表网格布局控件显示

--- app/src/main/java/com/neworin/easynotes/binding/NoteBookItemBindingAdapter.java
+++ app/src/main/java/com/neworin/easynotes/binding/NoteBookItemBindingAdapter.java
@@ -1,6 +1,7 @@
 package com.neworin.easynotes.binding;
 
 import android.databinding.BindingAdapter;
+import android.graphics.Color;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -8,6 +9,8 @@ import android.widget.TextView;
 import com.alibaba.fastjson.JSON;
 import com.bumptech.glide.Glide;
 import com.neworin.easynotes.model.EditData;
+import com.neworin.easynotes.model.Note;
+import com.neworin.easynotes.utils.DateUtil;
 
 import java.util.List;
 
@@ -71,5 +74,52 @@ public class NoteBookItemBindingAdapter {
             }
         }
         textView.setText(sb.toString());
+    }
+
+    @BindingAdapter("gridTextView")
+    public static void setGridTextView(TextView textView, String content) {
+        boolean isHaveImage = false;
+        StringBuilder sb = new StringBuilder();
+        List<EditData> list = JSON.parseArray(content, EditData.class);
+        if (null == list || list.size() == 0) {
+            textView.setVisibility(View.GONE);
+            return;
+        }
+        for (EditData e : list) {
+            if (e.getImagePath() == null) {
+                sb.append(e.getInputStr());
+            } else {
+                isHaveImage = true;
+            }
+        }
+        if (isHaveImage) {
+            textView.setTextColor(Color.WHITE);
+        } else {
+            textView.setTextColor(Color.BLACK);
+            textView.setSingleLine(false);
+        }
+        textView.setText(sb.toString());
+    }
+
+    @BindingAdapter("timeTextView")
+    public static void setTimeTextView(TextView textView, Note note) {
+        boolean isHaveImage = false;
+        String content = note.getContent();
+        List<EditData> list = JSON.parseArray(content, EditData.class);
+        if (null == list || list.size() == 0) {
+            textView.setTextColor(Color.BLACK);
+            return;
+        }
+        for (EditData e : list) {
+            if (e.getImagePath() != null) {
+                isHaveImage = true;
+            }
+        }
+        textView.setText(DateUtil.formatFriendly(note.getUpdateTime()));
+        if (isHaveImage) {
+            textView.setTextColor(Color.WHITE);
+        } else {
+            textView.setTextColor(Color.BLACK);
+        }
     }
 }
